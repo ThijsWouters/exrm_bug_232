9f715c7d90fdefca7d890204080ae72fc6d0369b
Thijs Wouters <thijs@morewood.be>
2015-11-10 11:09:50 +0100

    Implement String.Chars for ExrmBug_232

--- lib/exrm_bug_232.ex
+++ lib/exrm_bug_232.ex
@@ -1,5 +1,14 @@
 defmodule ExrmBug_232 do
+  defstruct []
+
   def test do
     IO.puts :test
+    IO.puts %ExrmBug_232{}
+  end
+end
+
+defimpl String.Chars, for: ExrmBug_232 do
+  def to_string(_ignore) do
+    "test"
   end
 end
